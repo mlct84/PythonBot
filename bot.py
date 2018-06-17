41befe6f8fd6c322e065b4dc2a8c4e9a1639f952
mlct84 <2991324+mlct84@users.noreply.github.com>
2018-06-16 22:32:57 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,6 @@ async def purge(ctx):
         message_list.append(x)
 
     message_count = len(message_list)
-
     if message_count < 2:
         await bot.delete_message(x)
         print("Deleted ", message_count, " Message")
@@ -32,8 +31,17 @@ async def purge(ctx):
         await bot.delete_messages(message_list)
         print("Deleted ", message_count, " Messages")
 
-    message = "Cleared " + str(message_count) + " message(s) at " + time.strftime("%d/%m/%Y") + " " + \
-              time.strftime("%H:%M:%S")
-    bot.send_message(ctx.message.channel, message)
+    send_message = "Cleared " + str(message_count) + " message(s) at " + time.strftime("%d/%m/%Y") + " " + \
+                   time.strftime("%H:%M:%S")
+    await bot.send_message(ctx.message.channel, send_message)
+
+
+@bot.command(pass_context=True)
+async def dc(ctx):
+    if "457642531082076164" in [role.id for role in ctx.message.author.roles]:
+        await bot.logout()
+        await bot.close()
+    else:
+        await bot.send_message(ctx.message.channel, "Only admin can disconnect bot.")
 
 bot.run(api_key)
